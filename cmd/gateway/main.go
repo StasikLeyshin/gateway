78e0a48af20923493fb90d0a38b230912d99acb3
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-01-30 22:36:43 +0300

    fix cmd

---
+++ cmd/gateway/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"gateway/internal/app/startup"
+	"gateway/internal/service/service"
+	"gateway/internal/transfer"
+)
+
+func main() {
+
+	// Файл с конфигурацией проекта
+	configPath := "config/config.local.yaml" //os.Getenv("CONFIG_PATH")
+
+	// Создаём логгер
+	logger := startup.NewLogger()
+
+	// Парсим файл конфигурации
+	_, err := startup.NewConfig(configPath)
+	if err != nil {
+		logger.Fatalf("failed to Config: %v", err)
+	}
+
+	// Клиент для реализации бизнес-логики
+	_ = service.NewGlobalService(transfer.NewTransfer())
+
+	//// Реализация методов grpc
+	//implementationServer := role_service.NewRoleService(serviceClient)
+	//
+	//// Создаём экземпляр grpc сервера
+	//grpcClient := grpc.NewServerGRPC(config.GrpcConfig.Port, implementationServer, logger)
+	//
+	//// Запускаем компонент grpc сервера
+	//app.NewApp(logger, grpcClient).Run(context.Background())
+}
