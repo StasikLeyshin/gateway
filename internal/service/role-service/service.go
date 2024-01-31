b434e289a5f9cbea75caee2129528412615327e6
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-01-31 22:04:05 +0300

    fix service.go

--- internal/service/role-service/service.go
+++ internal/service/role-service/service.go
@@ -2,52 +2,11 @@ package role_service
 
 import "gateway/internal/service/service"
 
-//import def "gateway/internal/service"
-//
-//var _ def.RoleService = (*service)(nil)
-//
-//type service struct {
-//	//producer kafka.KafkaProducer
-//}
-//
-//func NewService(
-//// producer kafka.KafkaProducer,
-//) *service {
-//	return &service{
-//		//producer: producer,
-//	}
-//}
-
-//var _ service.RoleService = (*roleService)(nil)
-//
-//type roleService struct {
-//	//producer kafka.KafkaProducer
-//	service service.GlobalService
-//}
-//
-//func NewRoleService(service service.GlobalService) *roleService {
-//	return &roleService{
-//		service: service,
-//	}
-//}
-//
-//func (r *roleService) Login(ctx context.Context, request *LoginRequest) (*LoginResponse, error) {
-//	response, err := request.Do(ctx, r.service)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return response, nil
-//}
-
-//type RoleService interface {
-//	LoginService
-//}
-
 type roleService struct {
 	service *service.GlobalService
 }
 
-func New(service *service.GlobalService) *roleService {
+func NewRoleService(service *service.GlobalService) *roleService {
 	return &roleService{
 		service: service,
 	}
