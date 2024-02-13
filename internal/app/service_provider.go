932d000a3c742bc46034b611ff6f4da9830d3a4d
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-13 22:53:42 +0300

    fix service_provider.go

--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -1,11 +1,80 @@
 package app
 
-import "gateway/internal/app/startup"
+import (
+	roleServiceGRPC "gateway/internal/api/grpc-gateway/role-service"
+	"gateway/internal/app/startup"
+	roleService "gateway/internal/service/role-service"
+	"gateway/internal/service/service"
+	"gateway/internal/transfer"
+	desc "github.com/StasikLeyshin/libs-proto/grpc-gateway/role-service/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
 
 type serviceProvider struct {
-	config *startup.Config
+	config  *startup.Config
+	service *service.GlobalService
+
+	transfer transfer.Transfer
 }
 
 func newServiceProvider() *serviceProvider {
 	return &serviceProvider{}
+}
+
+func (s *serviceProvider) initConfig(configPath string) error {
+	config, err := startup.NewConfig(configPath)
+	if err != nil {
+		return err
+	}
+
+	s.config = config
+
+	return nil
+}
+
+func (s *serviceProvider) Transfer() transfer.Transfer {
+	if s.transfer == nil {
+		s.transfer = transfer.NewTransfer()
+	}
+
+	return s.transfer
+}
+
+func (s *serviceProvider) GlobalService() *service.GlobalService {
+	if s.service == nil {
+		s.service = service.NewGlobalService(
+			s.Transfer(),
+		)
+	}
+
+	return s.service
+}
+
+func (s *serviceProvider) RoleServiceImpl() *user.Implementation {
+	if s.userImpl == nil {
+		s.userImpl = roleServiceGRPC.NewImplementationRoleService(s.GlobalService())
+	}
+
+	return s.userImpl
+}
+
+func (s *serviceProvider) Registration() {
+	desc.RegisterRoleServiceServer(s, roleServiceGRPC.NewImplementationRoleService(roleService.NewRoleService(service)))
+}
+
+func (s *serviceProvider) initGRPCServer() *grpc.Server {
+	//if s.service == nil {
+	//	s.service = service.NewGlobalService(
+	//		s.Transfer(),
+	//	)
+	//}
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+
+	reflection.Register(grpcServer)
+
+	Registration(grpcServer, service)
+
+	return s.service
 }
