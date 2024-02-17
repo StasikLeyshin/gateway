e86eb2ea92b74c21686c6c9364a318beae313906
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-17 23:48:11 +0300

    fix service_provider.go

--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -3,33 +3,43 @@ package app
 import (
 	roleServiceGRPC "gateway/internal/api/grpc-gateway/role-service"
 	"gateway/internal/app/startup"
+	server "gateway/internal/server/grpc"
 	roleService "gateway/internal/service/role-service"
 	"gateway/internal/service/service"
 	"gateway/internal/transfer"
 	desc "github.com/StasikLeyshin/libs-proto/grpc-gateway/role-service/pb"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
 type serviceProvider struct {
+	logger *logrus.Logger
+
 	config  *startup.Config
 	service *service.GlobalService
+
+	grpcServer *grpc.Server
 
 	transfer transfer.Transfer
 }
 
-func newServiceProvider() *serviceProvider {
-	return &serviceProvider{}
+func newServiceProvider(logger *logrus.Logger) *serviceProvider {
+	return &serviceProvider{
+		logger: logger,
+	}
 }
 
 func (s *serviceProvider) initConfig(configPath string) error {
-	config, err := startup.NewConfig(configPath)
-	if err != nil {
-		return err
-	}
+	if s.config == nil {
+		config, err := startup.NewConfig(configPath)
+		if err != nil {
+			return err
+		}
 
-	s.config = config
+		s.config = config
+	}
 
 	return nil
 }
@@ -52,29 +62,45 @@ func (s *serviceProvider) GlobalService() *service.GlobalService {
 	return s.service
 }
 
-func (s *serviceProvider) RoleServiceImpl() *user.Implementation {
+func (s *serviceProvider) RoleServiceImpl() *roleServiceGRPC.RoleService {
 	//if s.userImpl == nil {
-	//	s.userImpl = roleServiceGRPC.NewImplementationRoleService(s.GlobalService())
+	//	s.userImpl = roleServiceGRPC.NewImplementationRoleService(roleService.NewRoleService(s.GlobalService()))
 	//}
 
-	return s.userImpl
+	return roleServiceGRPC.NewImplementationRoleService(roleService.NewRoleService(s.GlobalService())) //s.userImpl
 }
 
-func (s *serviceProvider) Registration() {
-	desc.RegisterRoleServiceServer(s, roleServiceGRPC.NewImplementationRoleService(roleService.NewRoleService(service)))
+func (s *serviceProvider) Registration(reg grpc.ServiceRegistrar) {
+	desc.RegisterRoleServiceServer(reg, s.RoleServiceImpl())
 }
 
 func (s *serviceProvider) initGRPCServer() *grpc.Server {
-	//if s.service == nil {
-	//	s.service = service.NewGlobalService(
-	//		s.Transfer(),
-	//	)
-	//}
-	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	if s.grpcServer == nil {
+		s.grpcServer = grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
 
-	reflection.Register(grpcServer)
+		reflection.Register(s.grpcServer)
 
-	Registration(grpcServer, service)
+		s.Registration(s.grpcServer)
+	}
 
-	return s.service
+	return s.grpcServer
+}
+
+//func (s *serviceProvider) initGRPCServer1() *grpc.Server {
+//	if s.serverGrpc == nil {
+//		s.serverGrpc = grpcServer.NewServerGRPC(config.GrpcConfig.Port, serviceClient, logger)
+//	}
+//
+//	s.serverGrpc =
+//
+//	return serverGrpc
+//}
+
+func (s *serviceProvider) initComponents() []component {
+
+	components := []component{
+		server.NewServerGRPC(s.grpcServer, s.logger),
+	}
+
+	return components
 }
