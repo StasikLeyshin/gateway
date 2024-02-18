c22bc57ee3a3041f953334f653a2131569b076fb
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-18 23:29:35 +0300

    fix service.go

--- internal/service/service/service.go
+++ internal/service/service/service.go
@@ -1,12 +1,13 @@
 package service
 
 import (
+	"gateway/internal/service"
 	"gateway/internal/transfer"
 )
 
 type GlobalService struct {
-	transfer transfer.Transfer
-	//microServices service.MicroServices
+	transfer      transfer.Transfer
+	microServices service.MicroServices
 }
 
 func NewGlobalService(transfer transfer.Transfer) *GlobalService {
@@ -14,6 +15,11 @@ func NewGlobalService(transfer transfer.Transfer) *GlobalService {
 		transfer: transfer,
 	}
 }
+
+//func (g *GlobalService) GetServer(ctx context.Context) error {
+//	role_service.NewRoleService(g)
+//	return nil
+//}
 
 //func (g *GlobalService) Login(ctx context.Context, request *role_service.LoginRequest) (*role_service.LoginResponse, error) {
 //	return request.Do(ctx, g)
