683bc4d4b0177309a33262271dda5473ac383675
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-19 23:04:15 +0300

    add components.go

---
+++ pkg/components/components.go
@@ -0,0 +1,49 @@
+package components
+
+import "context"
+
+const (
+	Stopped Status = 0
+	Started Status = 1
+)
+
+type (
+	//Starter interface {
+	//	Start() error
+	//}
+	//
+	//Stopper interface {
+	//	Stop(ctx context.Context) error
+	//}
+	//
+	//Configurator[Config any] interface {
+	//	Configure(ctx context.Context, config Config) error
+	//}
+
+	Status int
+
+	Configurator[Config any] interface {
+		Start() error
+		Stop(ctx context.Context) error
+		Configure(ctx context.Context, config Config) error
+	}
+
+	ComponentFunc[Config any] func(ctx context.Context, config Config) error
+
+	Component[Config any] struct {
+		configurator Configurator[Config]
+		status       Status
+	}
+
+	Components[Config any] struct {
+		Components []Component[Config]
+	}
+)
+
+func (c *Components[Config]) Add() {
+
+}
+
+func AddComponent[Config any](configurator Configurator[Config]) {
+
+}
