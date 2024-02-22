d31b5a74e5ed2525e4988fd389a2fa6a18cf69c1
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-22 22:35:41 +0300

    fix components.go

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -36,14 +36,39 @@ type (
 	}
 
 	Components[Config any] struct {
-		Components []Component[Config]
+		Components []*Component[Config]
 	}
 )
 
-func (c *Components[Config]) Add() {
+func (c *Components[Config]) Add(configurator Configurator[Config]) {
+	component := &Component[Config]{
+		configurator: configurator,
+		status:       Stopped,
+	}
+
+	c.Components = append(c.Components, component)
 
 }
 
-func AddComponent[Config any](configurator Configurator[Config]) {
+func AddComponent[
+Config any,
+component
+](
+	components Components[Config],
+	config Configurator[Config],
+) {
+	//components.Add(config)
+	components.Add(func(ctx context.Context, config Config, init bool) error {
+		return module.Configure(ctx, adapter(config), init)
+	})
+}
 
+func Add[Config any, SubConfig any, Group intoGroup[Config], Module Configurator[SubConfig]](
+	group Group,
+	module Module,
+	adapter ConfigAdapter[Config, SubConfig],
+) {
+	group.intoGroup().AddModule(module, func(ctx context.Context, config Config, init bool) error {
+		return module.Configure(ctx, adapter(config), init)
+	})
 }
