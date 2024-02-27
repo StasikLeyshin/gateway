75e9aa8bec426eb98456a4439f4b87726434d68e
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-02-27 23:54:00 +0300

    fix service.go

--- internal/transfer/grpc/manage-server-service/service.go
+++ internal/transfer/grpc/manage-server-service/service.go
@@ -8,7 +8,7 @@ import (
 type transferManageServerService struct {
 }
 
-func NewTransferRoleService() *transferManageServerService {
+func NewManageServerService() *transferManageServerService {
 	return &transferManageServerService{}
 }
 
