8e955f707b31b8acad84c6e92fd6dac51b068ad0
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-03-06 23:54:37 +0300

    fix login_converter.go

--- internal/service/role-service/model/login_converter.go
+++ internal/service/role-service/model/login_converter.go
@@ -1,13 +1,21 @@
 package model
 
 import (
-	descExternal "github.com/StasikLeyshin/libs-proto/grpc-gateway/role-service/pb"
+	desc "github.com/StasikLeyshin/libs-proto/grpc-gateway/role-service/pb"
 )
 
-func (convert *LoginRequest) ToService(value *descExternal.LoginRequest) *LoginRequest {
+func (convert *LoginRequest) ToService(value *desc.LoginRequest) *LoginRequest {
 	return nil
 }
 
-func (convert *LoginResponse) ToService(value *descExternal.LoginResponse) *LoginResponse {
+func (convert *LoginRequest) FromService(value *desc.LoginRequest) *desc.LoginRequest {
+	return nil
+}
+
+func (convert *LoginResponse) ToService(value *desc.LoginResponse) *LoginResponse {
+	return nil
+}
+
+func (convert *LoginResponse) FromService(value *desc.LoginResponse) *desc.LoginResponse {
 	return nil
 }
