6e466d68e0b89ac838f094d6bc07c7c74b0fba0b
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-03-13 23:31:47 +0300

    fix components.go

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -137,6 +137,7 @@ func (c *Components[Config]) Configure(ctx context.Context, config Config) error
 			return c.actionError(component, "component configuration error", err)
 			//errors = append(errors, c.actionError(component, "component configuration error", err))
 		} else {
+			//errors = append(errors, c.checkStatus(component, Stopped))
 		}
 	}
 	return nil
