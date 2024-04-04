01530e26d3e35c0adcf3d6c42cd161a3ecb80c8f
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-04-04 23:43:23 +0300

    fix service.go

--- internal/service/role-service/service.go
+++ internal/service/role-service/service.go
@@ -1,13 +1,11 @@
 package role_service
 
-import "gateway/internal/service"
-
 type roleService struct {
-	internalService service.InternalService
+	//internalService service.InternalService
 }
 
-func NewRoleService(internalService service.InternalService) *roleService {
-	return &roleService{
-		internalService: internalService,
-	}
-}
+//func NewRoleService(internalService service.InternalService) *roleService {
+//	return &roleService{
+//		internalService: internalService,
+//	}
+//}
