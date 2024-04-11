884a68d23896e7e35814a6764025f219d2040215
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-04-11 22:26:57 +0300

    fix service.go

--- internal/service/service/service.go
+++ internal/service/service/service.go
@@ -25,16 +25,16 @@ func (g *internalService) GetTransfer() transfer.Transfer {
 	return g.transfer
 }
 
-//func (g *GlobalService) GetServer(ctx context.Context) error {
-//	role_service.NewRoleService(g)
-//	return nil
-//}
+func (g *GlobalService) GetServer(ctx context.Context) error {
+	role_service.NewRoleService(g)
+	return nil
+}
 
-//func (g *GlobalService) GetServer(ctx context.Context) error {
-//	role_service.NewRoleService(g)
-//	return nil
-//}
+func (g *GlobalService) GetServer(ctx context.Context) error {
+	role_service.NewRoleService(g)
+	return nil
+}
 
-//func (g *GlobalService) Login(ctx context.Context, request *role_service.LoginRequest) (*role_service.LoginResponse, error) {
-//	return request.Do(ctx, g)
-//}
+func (g *GlobalService) Login(ctx context.Context, request *role_service.LoginRequest) (*role_service.LoginResponse, error) {
+	return request.Do(ctx, g)
+}
