73502ec075cf8ebfd5aab685623971921c7fbc95
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-10-08 22:18:08 +0300

    add transfer

--- internal/transfer/interface.go
+++ internal/transfer/interface.go
@@ -20,3 +20,49 @@ type RoleTransfer interface {
 type Transfer interface {
 	RoleTransfer
 }
+
+func sendRequestToBSOrGate[
+	ServiceRequest any,
+	ServiceResponse any,
+](
+	ctx context.Context,
+	client any,
+
+) {
+}
+
+type serviceFunc[Req, Resp any] func(ctx context.Context, request Req) (Resp, error)
+
+func CallService[
+	GrpcRequest any,
+	GrpcResponse any,
+	ServiceRequestBase any,
+	// ServiceResponse fromServiceToApi[GrpcResponse, GrpcResponse],
+	// ServiceRequest toServiceFromApi[GrpcRequest, ServiceRequest],
+	ServiceResponse interface {
+		FromService() GrpcResponse
+	},
+	ServiceRequest interface {
+		ToService(value GrpcRequest) ServiceRequest
+		*ServiceRequestBase
+	},
+](
+	ctx context.Context,
+	request GrpcRequest,
+	fn serviceFunc[ServiceRequest, ServiceResponse], //func(context.Context, ServiceRequest) (ServiceResponse, error),
+	resp GrpcResponse,
+	// re ServiceRequest,
+) (GrpcResponse, error) {
+	//serviceRequest := re.ToService(request)
+	serviceRequest := ServiceRequest(new(ServiceRequestBase)).ToService(request)
+
+	serviceResponse, err := fn(ctx, serviceRequest)
+
+	if err != nil {
+		return *new(GrpcResponse), nil //, HandleAppError(err)
+	}
+
+	resp = serviceResponse.FromService()
+
+	return resp, nil
+}
