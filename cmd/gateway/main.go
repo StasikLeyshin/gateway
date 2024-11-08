5e1d7be50b1d2c4999fd0027decf52ff79de0b70
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2024-11-08 23:57:04 +0300

    fix deploy

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -4,12 +4,25 @@ import (
 	"context"
 	"gateway/internal/app"
 	"gateway/internal/app/configuration"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 
+	configFileName := "config.local.yaml"
+	configFolder := "config"
+
+	if IsDeploy := os.Getenv("IS_DEPLOY"); IsDeploy != "" {
+		configFileName = "config.yaml"
+	}
+
+	if path := os.Getenv("CONFIG_PATH"); path != "" {
+		configFolder = path
+	}
+
 	// Файл с конфигурацией проекта
-	configPath := "config/config.local.yaml" //os.Getenv("CONFIG_PATH")
+	configPath := filepath.Join(configFolder, configFileName)
 
 	// Создаём логгер
 	logger := configuration.NewLogger()
